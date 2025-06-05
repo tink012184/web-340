c6dfc0a58fe18f9120e137e3972bd843acd427b1
Tink <85289337+tink012184@users.noreply.github.com>
2025-06-05 12:47:47 -0500

    week 2

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,14 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
-*/
+ * Author:Melissa Lutz
+ * Date:6/3/2025
+ * File Name:Melissa Lutz
+ * Description:Simple recipe app with timers
+ */
 
 // TODO: Import your module using require
+const { createRecipe, setTimer, quit } = require("./recipes");
 
 // TODO: Implement your CLI program here
+console.log(createRecipe(["flour", "sugar", "eggs"]));
+console.log(setTimer(15));
+console.log(quit());
--- week-2/cooking-app/recipes.js
+++ week-2/cooking-app/recipes.js
@@ -1,23 +1,31 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
-*/
+ * Author:Melissa Lutz
+ * Date:6/5/2025
+ * File Name:recipes.js
+ * Description:Simple recipe app with timers
+ */
 
 // Define the createRecipe function
 function createRecipe(ingredients) {
   // TODO: Implement this function
+  return `Recipe created with ingredients: ${ingredients.join(", ")}`;
 }
 
 // Define the setTimer function
 function setTimer(minutes) {
   // TODO: Implement this function
+  return `Timer set for ${minutes} minutes`;
 }
 
 // Define the quit function
 function quit() {
   // TODO: Implement this function
+  return "Program exited";
 }
 
 // TODO: Export the functions
+module.exports = {
+  createRecipe,
+  setTimer,
+  quit,
+};
